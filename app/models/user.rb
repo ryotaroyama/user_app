a8bd675c7bdfbc6319401294d4ed7e34504e090a
ryotaroyamashita <ryotaro@ryotaroMacBook-Air.local>
2021-02-20 21:37:23 +0900

    User モデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 256 }, uniqueness: true
+  validates :age, presence: true, length: { in: 0..150 }
 end
