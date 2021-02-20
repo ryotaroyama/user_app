94f11868d42f8d14474f18e3290897f9f1566e27
ryotaroyamashita <ryotaro@ryotaroMacBook-Air.local>
2021-02-20 12:28:49 +0900

    コントローラの処理をbefore_actionで共通化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
     @users = User.order(id: :asc)
   end
 
   def show
-    @user = User.find(params[:id])
+
   end
 
   def new
@@ -17,22 +20,24 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    user = User.find(params[:id])
-    user.update!(user_params)
-    redirect_to user
+    @user.update!(user_params)
+    redirect_to @user
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy!
+    @user.destroy!
     redirect_to root_path
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :age)
