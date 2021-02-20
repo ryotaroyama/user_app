ff6f427ea5309bfa9f1887b0cf22289f70487640
ryotaroyamashita <ryotaro@ryotaroMacBook-Air.local>
2021-02-21 00:52:02 +0900

    バリデーションエラーメッセージを表示できるように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 256 }, uniqueness: true
-  validates :age, presence: true, length: { in: 0..150 }
+  validates :age, presence: true, numericality: { greater_than_or_equal_to:0, less_than_or_equal_to:150 }
 end
